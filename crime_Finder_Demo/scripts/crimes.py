853d17a3b2d715fe80ffaf9d7e405bc4abf10496
mnaka <motoki.nakamura@mail.mcgill.ca>
2017-02-05 01:17:29 -0500

    data files

---
+++ crime_Finder_Demo/scripts/crimes.py
@@ -0,0 +1,12 @@
+# Convert a massive csv into a json file
+import pandas as pd
+# Use pandas to read the csv quickly
+
+if __name__ == "__main__":
+    df = pd.read_csv("crimes.csv")
+    df['DATE'] = df.DATE.str.split(' ').str[0]
+    df.to_json("crimes.json", orient="index")
+
+    # df = pd.read_csv("data/Parking_Tags_Data_2015_1.csv")
+    df_subset = df.iloc[0:100]
+    df_subset.to_json("subset_test.json", orient="index")
